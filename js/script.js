27fbc7f35f10ab6eaaa3c82333019ef31bf05683
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-06 09:16:39 +0000

    added html, js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yoochan
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+function ShowAnswer() {
+  const userage = document.getElementById("get_age").value
+  const Tuesday = document.getElementById("option-2").checked
+  const Thursday = document.getElementById("option-4").checked
+  const discount = document.getElementById("option-discount1").checked
+  //get user's answer
+  if ((userage > 12 && userage < 21) || (Tuesday == 1 || Thursday == 1) || (discount == 1)) {
+    document.getElementById("TheAnswer").innerHTML = "You can get a discount today!"
+  }else {
+    document.getElementById("TheAnswer").innerHTML = "There is no ticket discount today or you are not eligible."
+  }
+}
