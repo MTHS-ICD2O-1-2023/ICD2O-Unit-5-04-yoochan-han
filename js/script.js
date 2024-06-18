e2b5cd0d11312da8a0cad9bfe9a36c1183ecd8bf
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-06-18 21:20:13 +0000

    debuged

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@
 // Created on: Apr 2024
 // This file contains the JS functions for index.html
 function ShowAnswer() {
+  "use strict"
   const userage = document.getElementById("get_age").value
   const Tuesday = document.getElementById("option-2").checked
   const Thursday = document.getElementById("option-4").checked
-  const discount = document.getElementById("option-discount1").checked
   //get user's answer
-  if ((userage > 12 && userage < 21) || (Tuesday == 1 || Thursday == 1) && (discount == 1)) {
+  if ((userage > 12 && userage < 21) || (Tuesday == 1 || Thursday == 1)) {
     document.getElementById("TheAnswer").innerHTML = "You can get a discount today!"
   }else {
-    document.getElementById("TheAnswer").innerHTML = "There is no ticket discount today or you are not eligible."
+    document.getElementById("TheAnswer").innerHTML = "There is no ticket discount today."
   }
 }
